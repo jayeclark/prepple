9d17faaa5b879006248a33998a0405d7949c4318
Jay Clark <jay@jayeclark.dev>
2022-06-14 14:28:00 -0400

    Update cdk stack environment vars

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,11 @@ import { DeploymentPipeline } from "./DeploymentPipeline";
 
 const app = new App();
 
-const pipeline = new DeploymentPipeline(app, 'DeploymentPipeline', {})
+const pipeline = new DeploymentPipeline(app, 'DeploymentPipeline', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
+})
 
 app.synth();
