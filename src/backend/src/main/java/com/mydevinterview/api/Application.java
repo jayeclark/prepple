463dc7b825f22db23221124ffeaa7bc4a8beb0e6
Jay Clark <jay@jayeclark.dev>
2022-08-13 18:05:48 -0700

    Install archetype project from awslabs
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

---
+++ src/backend/src/main/java/com/mydevinterview/api/Application.java
@@ -0,0 +1,19 @@
+package com.mydevinterview.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.mydevinterview.api.controller.PingController;
+
+
+@SpringBootApplication
+// We use direct @Import instead of @ComponentScan to speed up cold starts
+// @ComponentScan(basePackages = "com.mydevinterview.api.controller")
+@Import({ PingController.class })
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
