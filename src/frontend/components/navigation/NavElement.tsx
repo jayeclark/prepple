932f79dbf481b6ad364b244e844bd66c1b0573a1
Jay Clark <jay@jayeclark.dev>
2023-04-15 10:31:03 -0700

    Create Nav Element
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

--- src/frontend/components/navigation/NavElement.tsx
+++ src/frontend/components/navigation/NavElement.tsx
@@ -6,23 +6,24 @@ import { NavElementProps } from "./types";
 import styles from "../../styles/Nav.module.css";
 
 
-export function NavElement({path, setActivePage, name, Icon}: NavElementProps) {
+export function NavElement({path, setActivePage, name, Icon, user}: NavElementProps) {
   const theme = useTheme();
   const router = useRouter();
+  console.log(user?.jwt);
 
   return (
     <div className={router.pathname == path ? styles.navItemActive : styles.navItem} onClick={() => setActivePage(path)}>
       <div className={styles.navItemContainer}>
           <Link href={path} passHref>
             <div className={styles.navLink}>
-              <div className={styles.optionIcon}> 
-                  <SvgIcon
-                    Icon={Icon!}
-                    width={22}
-                    height={22}
-                    fillColor={theme.palette.background.default}
-                  />
-              </div>
+            {user?.jwt == undefined ? null : (<div className={styles.optionIcon}>
+              <SvgIcon
+                Icon={Icon!}
+                width={22}
+                height={22}
+                fillColor={theme.palette.background.default}
+              />
+            </div>)}
             <div className={styles.optionLabel}>{name}</div>
             </div>
         </Link>
