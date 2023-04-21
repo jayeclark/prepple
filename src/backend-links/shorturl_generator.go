5e11bd526d25008d59af301f08170dce62edaad1
Jay Clark <jay@jayeclark.dev>
2023-04-21 15:15:07 -0700

    Create short url generator
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

---
+++ src/backend-links/shorturl_generator.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"os"
+
+	"github.com/itchyny/base58-go"
+)
+
+func sha2560f(input string) []byte {
+	algorithm := sha256.New()
+	algorithm.Write([]byte(input))
+	return algorithm.Sum(nil)
+}
+
+func base58Encoded(bytes []byte) string {
+	encoding := base58.BitcoinEncoding
+	encoded, err := encoding.Encode(bytes)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	return string(encoded)
+}
