593c59ccbd2329853fcf7fdd0fe4d3d755d2c0fd
Jay Clark <jay@jayeclark.dev>
2023-05-26 15:30:48 -0400

    Update question model

--- src/backend/src/main/java/com/prepple/api/model/Question.java
+++ src/backend/src/main/java/com/prepple/api/model/Question.java
@@ -1,13 +1,35 @@
 package com.prepple.api.model;
 
 import lombok.Data;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+
+import javax.persistence.*;
+import java.sql.Time;
 
 @Entity
 @Data
 public class Question {
     @Id
+    @Column(name="question_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
+
+    private String title;
+
     private String question;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    private Question parent;
+
+    private double acceptance;
+
+    private double variation;
+
+    private double frequency;
+
+    @Column(name="created_at")
+    private Time createdAt;
+
+    @Column(name="updated_at")
+    private Time updatedAt;
 }
