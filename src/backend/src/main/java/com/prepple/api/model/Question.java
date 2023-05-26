9b4905081801db33a3d32f0c5d9e61296bb62695
Jay Clark <jay@jayeclark.dev>
2023-05-26 15:47:29 -0400

    Remove wildcard import

--- src/backend/src/main/java/com/prepple/api/model/Question.java
+++ src/backend/src/main/java/com/prepple/api/model/Question.java
@@ -2,7 +2,13 @@ package com.prepple.api.model;
 
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import java.sql.Time;
 
 @Entity
