0d596bc3857a24fa7e3e070f6bd97985f07f1a16
Jay Clark <jay@jayeclark.dev>
2023-06-06 14:56:37 -0700

    Add subscription enum
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

---
+++ src/backend/src/main/java/com/prepple/api/model/Subscription.java
@@ -0,0 +1,12 @@
+package com.prepple.api.model;
+
+public enum Subscription {
+    NONE,
+    FREEMIUM,
+    BASIC,
+    PREMIUM,
+    RECRUITER_INDIVIDUAL,
+    RECRUITER_ENTERPRISE,
+    ADMIN,
+    SUPERADMIN
+}
