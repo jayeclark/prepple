7758748793b7db40d9476f1b45c5f5219e4c67f8
Jay Clark <jay@jayeclark.dev>
2023-06-11 10:09:23 -0700

    Add author profile model

---
+++ src/backend/src/main/java/com/prepple/api/model/AuthorProfile.java
@@ -0,0 +1,51 @@
+package com.prepple.api.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;;
+import java.io.Serializable;
+import java.sql.Time;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthorProfile implements Serializable {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NonNull
+    @Column(unique=true)
+    private String urn;
+
+    @OneToOne
+    @JoinColumn(name="user_id", referencedColumnName="id")
+    @JsonProperty("userId")
+    private User user;
+
+    @NonNull
+    @Column(name="display_name")
+    private String displayName;
+
+    @NonNull
+    private String bio;
+
+    @Column(name="created_at")
+    @NonNull
+    private Time createdAt;
+
+    @Column(name="updated_at")
+    private Time updatedAt;
+}
