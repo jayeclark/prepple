4d41b3edc4d46ee995e8e9d44bb7a6750b404214
Jay Clark <jay@jayeclark.dev>
2023-06-21 16:45:33 -0700

    Add Favorite model

---
+++ src/backend/src/main/java/com/prepple/api/model/shared/EntityType.java
@@ -0,0 +1,10 @@
+package com.prepple.api.model.shared;
+
+public enum EntityType {
+    USER,
+    MENTOR_PROFILE,
+    QUESTION,
+    AUTHOR_PROFILE,
+    ANSWER,
+    GUIDE
+}
---
+++ src/backend/src/main/java/com/prepple/api/model/shared/Favorite.java
@@ -0,0 +1,46 @@
+package com.prepple.api.model.shared;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.sql.Time;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Favorite implements Serializable {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NonNull
+    private String favoriteId;
+
+    @NonNull
+    private EntityType entityType;
+
+    @ManyToOne
+    @JoinColumn(name="user_id", referencedColumnName="id")
+    @JsonProperty("userId")
+    private User user;
+
+    @Column(name="created_at")
+    @NonNull
+    private Time createdAt;
+
+    @Column(name="updated_at")
+    private Time updatedAt;
+}
