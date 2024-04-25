5c7ba1ed95461aee825c42dd4ca1cb2a86bde486
Jay Clark <jay@jayeclark.dev>
2024-04-24 18:31:35 -0700

    Fix build
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,10 +4,10 @@
 echo "Building backend..."
 cd src/backend && sam build && cd ../..
 
+# Build quarkus backend
+echo "Building quarkus backend..."
+cd src/backend-core && quarkus build && cd ../..
+
 # Build frontend
 echo "Building frontend..."
 cd src/frontend && yarn install && yarn run build && cd ../..
-
-# Build infrastructure after all the previous builds have completed
-# echo "Building infrastructure..."
-# npm run tsc src/infrastructure/*/* --outDir ./build
